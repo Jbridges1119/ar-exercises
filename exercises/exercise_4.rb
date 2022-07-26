38c95d0e2b194ff024ab52d6ea271be93d84ff2b
Jeff Bridges <jbridges1119@gmail.com>
2022-07-26 00:45:48 +0000

    Add FK for employees table  and populate

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,14 +6,19 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-stores = Store.create(name: "Surrey",annual_revenue: 224000, mens_apparel: false, womens_apparel: true,)
-stores = Store.create(name: "Whistler",annual_revenue: 1900000, mens_apparel: true, womens_apparel: false,)
-stores = Store.create(name: "Yaletown",annual_revenue: 430000, mens_apparel: true, womens_apparel: true,)
+stores = Store.create(name: "Surrey",annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+stores = Store.create(name: "Whistler",annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+stores = Store.create(name: "Yaletown",annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 
 @mens_apparel = Store.where(mens_apparel: true)
-puts @mens_apparel
+  puts @mens_apparel
 @mens_apparel.each do |store|
-  puts store.name
-  puts store.annual_revenue
+  puts store.name + " " + store.annual_revenue.to_s
+end
+
+@womens_apparel = Store.where("annual_revenue < 1000000", womens_apparel: true)
+puts @womens_apparel
+@womens_apparel.each do |store|
+  puts store.name + " " + store.annual_revenue.to_s
 end
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sue", last_name: "Pocket", hourly_rate: 60)
+@store2.employees.create(first_name: "Jack", last_name: "Johnson", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Jackson", hourly_rate: 60)
+p @store1.employees
+p @store2.employees
